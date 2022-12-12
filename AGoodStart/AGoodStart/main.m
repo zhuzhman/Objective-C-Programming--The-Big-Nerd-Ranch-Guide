43fde46e3a0539fd49240a4b5ce49e3a795b0c7f
Mikhail <33933295+zhuzhman@users.noreply.github.com>
2022-12-12 17:24:47 +0300

    AGoodStart

---
+++ AGoodStart/AGoodStart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AGoodStart
+//
+//  Created by Mikhail Zhuzhman on 12.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
